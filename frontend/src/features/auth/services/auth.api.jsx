75fc1ff711929af7f0229f0d9e3af7155f2ed685
madhu-1409 <madhu.y1409@gmail.com>
2026-08-27 02:47:05 +0530

    Update frontend for deployment

--- frontend/src/features/auth/services/auth.api.jsx
+++ frontend/src/features/auth/services/auth.api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
@@ -30,8 +30,8 @@ export async function login(email, password) {
 
 export async function logout() {
     try {
-        const response = await axios.get("http://localhost:3000/api/auth/logout", {
-            withCredentials: true
+        const response = await api.get("/api/auth/logout", {
+            
         })
         return response.data
     } catch (error) {
@@ -41,8 +41,8 @@ export async function logout() {
 }
 export async function getMe() {
     try {
-        const response = await axios.get("http://localhost:3000/api/auth/get-me", {
-            withCredentials: true
+        const response = await api.get("/api/auth/get-me", {
+            
         })
         return response.data
     } catch (err) {
